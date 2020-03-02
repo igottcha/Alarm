abf28f95f7ede83897edb054b4a1dd760f16f8a7
Chris Gottfredson <chrisgottfredson@gmail.com>
2020-03-02 16:05:25 -0700

    Model and Model Controller built out

---
+++ myAlarm/Alarm.swift
@@ -0,0 +1,12 @@
+//
+//  Alarm.swift
+//  myAlarm
+//
+//  Created by Chris Gottfredson on 3/2/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import Foundation
+class Alarm {
+    
+}
