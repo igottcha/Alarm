74cf2db0687de115881876ec4082fbebae8f7679
Chris Gottfredson <chrisgottfredson@gmail.com>
2020-03-02 16:35:49 -0700

    Model and Model Controller built out

--- myAlarm/Alarm.swift
+++ myAlarm/Alarm.swift
@@ -7,6 +7,33 @@
 //
 
 import Foundation
-class Alarm {
+
+class Alarm: Codable {
     
+    var fireDate: Date
+    var name: String
+    var enabled: Bool
+    let uuid: String
+    var fireTimeAsString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter.string(from: fireDate)
+    }
+    
+    init(fireDate: Date, name: String, enabled: Bool,uuid: String = UUID().uuidString) {
+        self.fireDate = fireDate
+        self.name = name
+        self.enabled = enabled
+        self.uuid = uuid
+    }
 }
+
+extension Alarm: Equatable {
+    static func == (lhs: Alarm, rhs: Alarm) -> Bool {
+        return  lhs.fireDate == rhs.fireDate &&
+                lhs.name == rhs.name &&
+                lhs.enabled == rhs.enabled &&
+                lhs.uuid == rhs.uuid
+    }
+}
+
---
+++ myAlarm/AlarmController.swift
@@ -0,0 +1,82 @@
+//
+//  AlarmController.swift
+//  myAlarm
+//
+//  Created by Chris Gottfredson on 3/2/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import Foundation
+
+class AlarmController {
+    
+    static let shared = AlarmController()
+    
+    var alarms: [Alarm] = []
+    
+    var mockAlarms: [Alarm] = {
+        let alarm1 = Alarm(fireDate: Date(), name: "Wake up", enabled: true)
+        let alarm2 = Alarm(fireDate: Date(), name: "Bedtime", enabled: true)
+        let alarm3 = Alarm(fireDate: Date(), name: "Call Mom", enabled: true)
+    
+    return [alarm1, alarm2, alarm3]
+    } ()
+        
+    init() {
+        self.alarms = self.mockAlarms
+        loadToPersistence()
+    }
+    
+    //MARK: - CRUD functions
+    
+    func addAlarm(fireDate: Date, name: String, enabled: Bool) -> Alarm {
+        let alarm = Alarm(fireDate: fireDate, name: name, enabled: enabled)
+        alarms.append(alarm)
+        saveToPersistence()
+        return alarm
+    }
+    
+    func updateAlarm(alarm: Alarm, fireDate: Date, name: String, enabled: Bool) {
+        alarm.fireDate = fireDate
+        alarm.name = name
+        alarm.enabled = enabled
+        saveToPersistence()
+    }
+    
+    func deleteAlarm(alarm: Alarm) {
+        guard let index = alarms.firstIndex(of: alarm) else {return}
+        alarms.remove(at: index)
+        saveToPersistence()
+    }
+    
+    //MARK: - Persistence
+    
+    func fileURL() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = path[0]
+        let fileName = "myAlarms.json"
+        let fullURL = documentDirectory.appendingPathComponent(fileName)
+        return fullURL
+    }
+    
+    func saveToPersistence() {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(alarms)
+            try data.write(to: fileURL())
+        } catch let error {
+            print("\(error.localizedDescription) -> \(error)")
+        }
+    }
+    
+    func loadToPersistence() {
+        let decoder = JSONDecoder()
+        do {
+            let data = try Data.init(contentsOf: fileURL())
+            let alarms = try decoder.decode([Alarm].self, from: data)
+            self.alarms = alarms
+        } catch let error {
+        print("\(error.localizedDescription) -> \(error)")
+        }
+    }
+}
