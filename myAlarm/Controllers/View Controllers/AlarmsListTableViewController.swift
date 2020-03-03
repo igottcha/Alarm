1d670e1c6e78cce06b706fe25f40c9f4b553b01e
Chris Gottfredson <chrisgottfredson@gmail.com>
2020-03-02 19:22:58 -0700

    Added View Controllers and SwitchTVC

---
+++ myAlarm/Controllers/View Controllers/AlarmsListTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AlarmsListTableViewController.swift
+//  myAlarm
+//
+//  Created by Chris Gottfredson on 3/2/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class AlarmsListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AlarmController.shared.alarms.count
+    }
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "alarmCell", for: indexPath) as? SwitchTableViewCell else {return UITableViewCell()}
+        let alarm = AlarmController.shared.alarms[indexPath.row]
+        cell.updateViews(alarm: alarm)
+        cell.delegate = self
+        return cell
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+}
+
+extension AlarmsListTableViewController: SwitchTableViewCellDelegate {
+    func alarmSwitchTapped(for cell: SwitchTableViewCell) {
+        guard let index = tableView.indexPath(for: cell) else {return}
+            let alarm = AlarmController.shared.alarms[index.row]
+        
+        cell.updateViews(alarm: alarm)
+    }
+}
---
+++ myAlarm/SwitchTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  SwitchTableViewCell.swift
+//  myAlarm
+//
+//  Created by Chris Gottfredson on 3/2/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+protocol SwitchTableViewCellDelegate: class {
+    func alarmSwitchTapped(for cell: SwitchTableViewCell)
+}
+
+class SwitchTableViewCell: UITableViewCell {
+
+    //MARK: - Outlets
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    //MARK: - Properties
+    
+    var alarm: Alarm? {
+        didSet {
+//            guard alarm != nil else {return}
+            updateViews(with: alarm!)
+        }
+    }
+    weak var delegate: SwitchTableViewCellDelegate?
+    
+    //MARK: - Actions
+    
+    @IBAction func switchValueChanged(_ sender: UISwitch) {
+    }
+    
+    //MARK: - Lifecycle
+   
+    //MARK: - Helper Functions
+    
+    func updateViews(with alarm: Alarm){
+        nameLabel.text = alarm.name
+        timeLabel.text = alarm.fireTimeAsString
+        alarmSwitch.isOn = alarm.enabled
+    }
+}
